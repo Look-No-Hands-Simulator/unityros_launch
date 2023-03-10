302f938166d3cddd441dc90620c4798dd3dc7370
fruffers <bywater30@gmail.com>
2023-03-10 15:25:21 +0000

    Fix all errors, some libraries needed to be imported.

--- launch/unityros_launch.py
+++ launch/unityros_launch.py
@@ -1,10 +1,14 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+from ament_index_python import get_package_share_directory
 from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution, TextSubstitution
 from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PythonExpression
 
 import socket
 
@@ -14,6 +18,7 @@ IPAddr = socket.gethostbyname(hostname)
 # You could use namespaces inside the nodes
 def generate_launch_description():
     return LaunchDescription([
+    
         DeclareLaunchArgument('ROS_IP', default_value=IPAddr, description='IP address of machine running ROS2.'),
         Node(
             package='unity_robotics_demo',
